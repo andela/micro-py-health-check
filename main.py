c9de19cf45358af0be6af83b9c1d32c5059a936c
Jackie W. Macharia <jackline.macharia@andela.com>
2017-06-07 13:15:00 +0300

    feat(main): setup Python health check code

---
+++ main.py
@@ -0,0 +1,36 @@
+"""Reference implementation for health checking in gRPC Python."""
+
+import threading
+
+import grpc
+
+import health_pb2
+import health_pb2_grpc
+
+
+class HealthServicer(health_pb2_grpc.HealthServicer):
+    """Servicer handling RPCs for service statuses."""
+
+    def __init__(self):
+        self._server_status_lock = threading.Lock()
+        self._server_status = {}
+
+    def Check(self, request, context):
+        with self._server_status_lock:
+            status = self._server_status.get(request.service)
+            if status is None:
+                context.set_code(grpc.StatusCode.NOT_FOUND)
+                return health_pb2.HealthCheckResponse()
+            else:
+                return health_pb2.HealthCheckResponse(status=status)
+
+    def set(self, service, status):
+        """Sets the status of a service.
+    Args:
+        service: string, the name of the service.
+            NOTE, '' must be set.
+        status: HealthCheckResponse.status enum value indicating
+            the status of the service
+    """
+        with self._server_status_lock:
+            self._server_status[service] = status
