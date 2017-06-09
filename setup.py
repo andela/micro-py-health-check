893a7ffc7ac035f9511db89e4b03d2ee3dd55798
Stephen Njoroge <steve254@Stephens-MacBook-Pro.local>
2017-06-09 15:31:18 +0300

    ft(setup) directs Distutils to package details

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup # Describe module distribution to Distutils
+
+setup(
+    name='micro-py-health-check',
+    packages=['micro-py-health-check'], # this must be the same as the name above
+    version='0.1',
+    description='A library to do rpc health checks on Python Services',
+    author='Andela',
+    author_email='steven.n360@gmail.com',
+    url='https://github.com/andela/micro-py-health-check/tree/ft-create-micro-health-check-python-package',
+    download_url='https://github.com/andela/micro-py-health-check/tree/ft-create-micro-health-check-python-package/archive/0.1.tar.gz',
+    # I'll explain this in a second
+    keywords=['testing', 'health checks', 'services', 'gRPC'], # arbitrary keywords
+    classifiers=[],
+)
